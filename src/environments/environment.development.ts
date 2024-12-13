57e6a73ffd1a3f87e4ebef370e08191a3308fb7c
hcolmenares <hcolmenares@corrosioncic.com>
2024-12-13 11:58:32 -0500

    Se arregla error env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,8 @@
 export const environment = {
   apiUrl: 'https://localhost:7236/api/',
   apiAuth: 'https://localhost:7236/api/Account/Login',
+  apiRegister: 'https://localhost:7236/api/Account/Register',
+
   localStorageTokenKey: 'base',
   units: [
     'Gramos',
